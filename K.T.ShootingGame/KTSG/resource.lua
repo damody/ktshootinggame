db8d3750887104c2c962f0d2e250038c6b201533
t1238142000 <t1238142000@d37ca794-c3e0-1400-3826-0723a5e6f173>
2011-09-02 21:34:32 +0000

    something move

--- K.T.ShootingGame/KTSG/resource.lua
+++ K.T.ShootingGame/KTSG/resource.lua
@@ -1,7 +1,11 @@
 pic = {
-{100, "pic/ball1.png"},
-{101, "pic/crate.jpg"},
-{102, "pic/main_warship.png"},
-{103, "pic/ray1.png"},
-{104, "pic/tower1.png"},
+{100, "Media/picture/ball1.png"},
+{101, "Media/picture/crate.jpg"},
+{102, "Media/picture/main_warship.png"},
+{103, "Media/picture/ray1.png"},
+{104, "Media/picture/tower1.png"},
+}
+
+wav = {
+{18, "Media/music/018.wav"}
 }
