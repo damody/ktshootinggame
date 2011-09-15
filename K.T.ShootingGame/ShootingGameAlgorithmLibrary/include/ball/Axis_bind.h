74fe9db49d011c6914ba6d3471f5075d5b4dcf42
t1238142000 <t1238142000@d37ca794-c3e0-1400-3826-0723a5e6f173>
2011-09-15 16:49:48 +0000

    finish collison part1

---
+++ K.T.ShootingGame/ShootingGameAlgorithmLibrary/include/ball/Axis_bind.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "ball/ball.h"
+
+struct Axis_bind
+{
+	float* x, *y;
+	Ball* ball;
+
+	Axis_bind(Ball* b)
+		:x(&(b->mPosition.x)), y(&(b->mPosition.y)), ball(b){}
+	Axis_bind(float *val)
+		:x(val), y(val), ball(0)
+	{}
+	bool operator == (Ball* b) const
+	{
+		return ball == b;
+	}
+	
+};
+bool Compare_axis_x (const Axis_bind& lhs, const Axis_bind& rhs);
+bool Compare_axis_y (const Axis_bind& lhs, const Axis_bind& rhs);
+// bool operator == (const Axis_bind& lhs, const Axis_bind& rhs);
+// bool operator != (const Axis_bind& lhs, const Axis_bind& rhs);
+// bool operator < (const Axis_bind& lhs, float rhs);
+// bool operator < (float lhs, const Axis_bind& rhs);
+typedef std::vector<Axis_bind> Axis_binds;
